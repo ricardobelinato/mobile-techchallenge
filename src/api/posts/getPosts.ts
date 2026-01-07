877c5cf5c3f1836cfc881c71e15c816837692a1f
Thatyanne Gomes <thatygomes57@gmail.com>
2026-01-07 00:57:04 -0300

    fix: update getPosts function to accept search parameter for filtering

--- src/api/posts/getPosts.ts
+++ src/api/posts/getPosts.ts
@@ -1,6 +1,6 @@
 import api from '../api';
 
-export async function getPosts() {
-  const response = await api.get('/posts');
+export async function getPosts(search?:string) {
+  const response = await api.get(`/posts?q=${search}`);
   return response.data;
 }
