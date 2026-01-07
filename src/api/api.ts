a230aed3dee48e377863e52e04f19a86e94822be
Thatyanne Gomes <thatygomes57@gmail.com>
2026-01-06 23:15:30 -0300

    feat: add request interceptor to handle authorization token

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,4 +9,20 @@ const api = axios.create({
     : `http://${LOCAL_IP}:3000`,
 });
 
-export default api;
+// Add a request interceptor
+api.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.log(config);
+    const token = localStorage.getItem("token"); 
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+  
+  export default api;
